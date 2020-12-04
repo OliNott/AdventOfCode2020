d267bd55579a9c6eea528c00d206ea5e77ac9ea6
Olivier Nottebart <olivier.nottebart@gmail.com>
2020-12-04 19:30:25 +0100

    Day 4 tests

--- Day4/solution.rb
+++ Day4/solution.rb
@@ -1,82 +1,92 @@
-def call
-  data = get_file_data(__dir__ + '/input.txt')
-  validations(data)
-end
-
 REQ = [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid]
 
-def validations(table)
-  p "Original length : #{table.length}"
-  table.filter! { |passport| (REQ - passport.keys).length === 0       }
-  table.filter! { |passport| valid_year?(passport[:byr], 1920, 2002) }
-  table.filter! { |passport| valid_year?(passport[:iyr], 2010, 2020) }
-  table.filter! { |passport| valid_year?(passport[:eyr], 2020, 2030) }
-  table.filter! { |passport| valid_height?(passport[:hgt])           }
-  table.filter! { |passport| valid_color?(passport[:hcl])            }
-  table.filter! { |passport| valid_eye?(passport[:ecl])              }
-  table.filter! { |passport| valid_pid?(passport[:pid])              }
-  p "Valid passports : #{table.length}"
-  table.length
-end
+class Passport
 
-def valid_year?(y, min, max)
-  return false unless y
-  return false if y.length != 4
+  def call(filename, verbose = false)
+    data = get_file_data(__dir__ + '/' + filename)
+    validations(data, verbose)
+  end
 
-  year = y.to_i
-  year >= min && year <= max 
-end
+  def validations(table, verbose)
+    if verbose then p "Original length : #{table.length}" end
 
-def valid_height?(h)
-  unit  = h.chars.last(2).join('')
-  value = h.chars.first(3).join('').to_i
+    table.filter! { |passport| valid_keys?(passport)                   }
+    table.filter! { |passport| valid_year?(passport[:byr], 1920, 2002) }
+    table.filter! { |passport| valid_year?(passport[:iyr], 2010, 2020) }
+    table.filter! { |passport| valid_year?(passport[:eyr], 2020, 2030) }
+    table.filter! { |passport| valid_height?(passport[:hgt])           }
+    table.filter! { |passport| valid_color?(passport[:hcl])            }
+    table.filter! { |passport| valid_eye?(passport[:ecl])              }
+    table.filter! { |passport| valid_pid?(passport[:pid])              }
 
-  if unit == 'cm'
-    value >=150 && value <= 193
-  elsif unit == 'in'
-    value >= 59 && value <= 76
-  else
-    false
+    if verbose then p "Valid passports : #{table.length}" end
+    table.length
+  end
+
+  def valid_keys?(passport)
+    (REQ - passport.keys).length === 0
+  end
+
+  def valid_year?(y, min, max)
+    return false unless y
+    return false if y.length != 4
+
+    year = y.to_i
+    year >= min && year <= max 
+  end
+
+  def valid_height?(h)
+    unit  = h.chars.last(2).join('')
+    value = h.chars.first(3).join('').to_i
+
+    if unit == 'cm'
+      value >=150 && value <= 193
+    elsif unit == 'in'
+      value >= 59 && value <= 76
+    else
+      false
+    end
+  end
+
+  def valid_color?(col)
+    return false unless col[0] == '#'
+
+    col[0] = ''
+    color = col.chars
+    valid = true
+    color.each { |char| valid = false unless [*('a'..'f'), *('0'..'9')].include?(char) }
+    valid
+  end
+
+  def valid_eye?(eye)
+    ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].include?(eye)
+  end
+
+  def valid_pid?(pid)
+    pid.length == 9
+  end
+
+  private
+
+  def get_file_data(filepath)
+    file = File.open(filepath)
+
+    # Breaking on empty newlines
+    data = file.read.split(/^\s*$\n/)
+    
+    # Replacing non empty new lines with regular spaces, then split
+    passports_string = data.map { |pass| pass.gsub(/\n/, ' ').split(' ') }
+    
+    # Building hash for each "key:value" string
+    passports_hash = passports_string.map do |line|
+      passport = {}
+      line.each { |pair| key, value = pair.split(/:/); passport[key.to_sym] = value }
+      passport
+    end
+
+    # Returning array of hashes
+    passports_hash
   end
 end
 
-def valid_color?(col)
-  return false unless col[0] == '#'
-
-  col[0] = ''
-  color = col.chars
-  valid = true
-  color.each { |char| valid = false unless [*('a'..'z'), *('0'..'9')].include?(char) }
-  valid
-end
-
-def valid_eye?(eye)
-  ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].include?(eye)
-end
-
-def valid_pid?(pid)
-  pid.length == 9
-end
-
-def get_file_data(filepath)
-  file = File.open(filepath)
-
-  # Breaking on empty newlines
-  data = file.read.split(/^\s*$\n/)
-  
-  # Replacing non empty new lines with regular spaces, then split
-  passports_string = data.map { |pass| pass.gsub(/\n/, ' ').split(' ') }
-  
-  # Building hash for each "key:value" string
-  passports_hash = passports_string.map do |line|
-    passport = {}
-    line.each { |pair| key, value = pair.split(/:/); passport[key.to_sym] = value }
-    passport
-  end
-
-  # Returning array of hashes
-  passports_hash
-end
-
-# Calling method
-call
+Passport.new.call('input.txt')
---
+++ Day4/test.rb
@@ -0,0 +1,115 @@
+require 'rspec/autorun'
+require_relative './solution'
+
+describe Passport do
+  validator = Passport.new
+
+  describe "Part 1" do
+    # KEY PRESENCE
+    describe "Presence of keys" do
+      it "Should be valid when keys are present" do
+        valids = validator.valid_keys?({
+          hcl: '#ae17e1',
+          iyr: 2013,
+          eyr: 2024,
+          ecl: 'brn',
+          pid: 760753108,
+          byr: 1931,
+          hgt: '179cm'
+        })
+        expect(valids).to be_truthy
+      end
+
+      it "Should not be valid when missing keys" do
+        valids = validator.valid_keys?({
+          hcl: '#cfa07d',
+          eyr: 2025,
+          pid: 166559648,
+          iyr: 2011,
+          ecl: 'brn',
+          hgt: '59in'
+        })
+        expect(valids).to be_falsey
+      end
+    end
+
+    describe "Part 1 overall test" do
+      it "Should pass part1 test file" do
+        valids = validator.call('test1.txt', false)
+        expect(valids).to eq(2)
+      end
+    end
+  end
+
+  describe "Part 2" do
+    
+    # BIRTHDATE
+    describe "Birthdate" do
+      it "Should accept valid birthyear" do
+        expect(validator.valid_year?('2002', 1920, 2002)).to be_truthy
+      end
+      it "Should reject unvalid birthyear" do
+        expect(validator.valid_year?('2003', 1920, 2002)).to be_falsey
+      end
+    end
+    
+    # HEIGHT
+    describe "Height" do
+      it "Should accept valid cm height" do
+        expect(validator.valid_height?('190cm')).to be_truthy
+      end
+      it "Should accept valid in height" do
+        expect(validator.valid_height?('60in')).to be_truthy
+      end
+      it "Should reject unvalid in height" do
+        expect(validator.valid_height?('190in')).to be_falsey
+      end
+      it "Should reject when nonpresent unit" do
+        expect(validator.valid_height?('190')).to be_falsey
+      end
+      it "Should reject when wrong unit" do
+        expect(validator.valid_height?('190fo')).to be_falsey
+      end
+    end
+    
+    # COlOR
+    describe "Color" do
+      it "Should accept when valid color" do
+        expect(validator.valid_color?('#123abc')).to be_truthy
+      end
+      it "Should reject when unallowed characater" do
+        expect(validator.valid_color?('#123abz')).to be_falsey
+      end
+      it "Should reject when missing '#'" do
+        expect(validator.valid_color?('123abc')).to be_falsey
+      end
+    end
+    
+    # EYE COLOR
+    describe "Eye color" do
+      it "Should accept when correct eye color" do
+        expect(validator.valid_eye?('brn')).to be_truthy
+      end
+      it "Should reject when wrong eye color" do
+        expect(validator.valid_eye?('wat')).to be_falsey
+      end
+    end
+
+    # PID
+    describe "PID" do
+      it "Should accept when correct PID" do
+        expect(validator.valid_pid?('000000001')).to be_truthy
+      end
+      it "Should reject when wrong PID" do
+        expect(validator.valid_pid?('0123456789')).to be_falsey
+      end
+    end
+
+    describe "Part 2 overall test" do
+      it "Should pass the part2 test file" do
+        valids = validator.call('test2.txt', false)
+        expect(valids).to eq(4)
+      end
+    end
+  end
+end
